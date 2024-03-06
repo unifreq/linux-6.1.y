1f2d969fe49b378a16adde00b4857ccdcf67e6c4
uniqfreq <flippy@sina.com>
2024-03-06 21:52:18 +0800

    arch: arm64: Apply the text_offset patch

--- arch/arm64/include/asm/boot.h
+++ arch/arm64/include/asm/boot.h
@@ -13,7 +13,8 @@
 #define MAX_FDT_SIZE		SZ_2M
 
 /*
- * arm64 requires the kernel image to placed at a 2 MB aligned base address
+ * arm64 requires the kernel image to placed
+ * TEXT_OFFSET bytes beyond a 2 MB aligned base
  */
 #define MIN_KIMG_ALIGN		SZ_2M
 
